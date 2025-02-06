dbd0149a050ae9ff73bf21a75bb4d00545ee33a6
yaracham <yaraelcham@hotmail.com>
2025-02-06 15:00:33 +0200

    AD- devoir 1

---
+++ AD-Dev1/src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/AD-dev1";
+        String user = "postgres";
+        String password = "Yayou2003";
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL database successfully!");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM users;");
+            int columnCount = rs.getMetaData().getColumnCount();
+            for (int i = 1; i <= columnCount; i++) {
+                System.out.print(rs.getMetaData().getColumnName(i) + "\t");
+            }
+            System.out.println("\n---------------------------------");
+            while (rs.next()) {
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.print(rs.getString(i) + "\t");
+                }
+                System.out.println();
+            }
+            rs.close();
+            stmt.close();
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
